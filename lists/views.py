a9a70d1e2fe39af6d723184c1aa21df3555d3225
amaestrello <alexandremaestrello@gmail.com>
2023-09-09 21:03:34 -0300

    Correcting view to deal with normal request and request via POST

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,7 @@ from .models import Item  # Importe o modelo Item do seu aplicativo
 def home_page(request):
     if request.method == 'POST':
         item_text = request.POST.get('item_text', '')
-        item_priority = request.POST.get('priority', '')  # Supondo que você tenha um campo 'priority' no formulário
+        item_priority = request.POST.get('priority', '') # Supondo que você tenha um campo 'priority' no formulário
         Item.objects.create(text=item_text, priority=item_priority)
 
     items = Item.objects.all()  # Recupere todos os itens da base de dados
